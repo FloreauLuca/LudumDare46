abfb486838b74d1cb280ecae91eabeccbaac7c51
FloreauLuca <lucafloreau@akeonet.com>
2020-04-18 18:46:20 +0200

    WWWAAAVVVESS

--- Assets/Scripts/RaftMovement.cs
+++ Assets/Scripts/RaftMovement.cs
@@ -12,10 +12,20 @@ public class RaftMovement : MonoBehaviour
     [SerializeField] private float lateralSpeed = 5.0f;
     [SerializeField] private PlayerMovement player;
 
+    private float sinusIndex;
+    [SerializeField] private float waveForceAngular = 1.0f;
+    [SerializeField] private float waveForceLinear = 1.0f;
+    [SerializeField] private Waves waves;
+    private Vector3 startPosition;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
+        sinusIndex = waves.SinusIndex + Mathf.Abs(transform.position.x - waves.transform.position.x) / waves.Speed;
+        startPosition = transform.position;
     }
 
     // Update is called once per frame
@@ -40,5 +50,13 @@ public class RaftMovement : MonoBehaviour
             player.transform.position = transform.position + Vector3.up;
             player.transform.rotation = transform.rotation;
         }
+
+        sinusIndex = waves.SinusIndex - Mathf.Abs(transform.position.x - waves.transform.position.x) / waves.Speed;
+
+        //Wave Movement
+        sinusIndex += Time.deltaTime;
+        rigidbody.angularVelocity += Mathf.Cos(sinusIndex*waves.WaveWidth)*waves.WaveHeight*waveForceAngular;
+        transform.position = new Vector3(transform.position.x, (Mathf.Sin(sinusIndex * waves.WaveWidth) * waves.WaveHeight * waveForceLinear) + startPosition.y);
+        Debug.Log(Mathf.Sin(sinusIndex * waves.WaveWidth) * waves.WaveHeight);
     }
 }
---
+++ Assets/Scripts/Waves.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waves : MonoBehaviour
+{
+    [SerializeField] private float waveHeight = 1.0f;
+    public float WaveHeight => waveHeight;
+    [SerializeField] private float waveWidth = 1.0f;
+    public float WaveWidth => waveWidth;
+    [SerializeField] private float speed = 1.0f;
+    public float Speed => speed;
+    [SerializeField] private GameObject wavesPrefab;
+    private float currentTimer;
+    private float sinusIndex;
+    public float SinusIndex => sinusIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTimer += Time.deltaTime;
+        sinusIndex += Time.deltaTime;
+        if (currentTimer > 1/speed)
+        {
+            currentTimer = 0;
+            GameObject gameObject = Instantiate(wavesPrefab, new Vector3(transform.position.x, transform.position.y + Mathf.Sin(sinusIndex*waveWidth)*waveHeight, transform.position.z), Quaternion.identity, transform);
+            gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.left * speed;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //if (other.CompareTag("Raft"))
+        //{
+        //    if (right)
+        //    {
+        //        other.GetComponent<Rigidbody2D>().angularVelocity -= force;
+        //    }
+        //    else
+        //    {
+        //        other.GetComponent<Rigidbody2D>().angularVelocity += force;
+        //    }
+        //}
+    }
+}
